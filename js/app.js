59843b200687b0e26ac4968be84ae60bd1c086de
Kevin Snider <kevin_snider@Kevins-MacBook-Pro.local>
2014-07-15 13:02:17 -0400

    Allowed users to choose which number would be fix or buzz
    If a user still inputs something that is not a number issues still
    occur.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,32 @@
-
+var fizz = prompt("Please enter your Fizz number")
+			
+			if(isNaN(fizz)||  fizz<=0 || fizz%1 !==0) {
+		 		alert('Please type in a positive whole number');
+		 	}
+		 
+var buzz = prompt("Please enter your Buzz number")
+	 		if(isNaN(buzz)||  buzz<=0 || buzz%1 !==0) {
+		 		alert('Please type in a positive whole number');
+		 	}
 
 var count =function (number){
 	/* Creates Count and determines what is devisable and will determine the Fizz Buzzes */
+	
 	for (i=1; i<=number; i++) {
-			if (i%3===0 && i%5===0) {
+			if (i%fizz===0 && i%buzz===0) {
 				$('.count').append('Fizzbuzz' + '<br>');
 			}
-			else if (i%3===0) {
+			else if (i%fizz===0) {
 				$('.count').append('Fizz' + '<br>');
 			}
-			else if (i%5===0) {
+			else if (i%buzz===0) {
 				$('.count').append('Buzz' + '<br>');
 			}
 			else {
 				$('.count').append(i + '<br>');
 			}
 		}
-	};
+};
 /*Allows users to input their number in the textbow */
 $('document').ready(function() {
 	$('.add-number').keydown(function(event){
@@ -40,32 +50,3 @@ $(document).on("click", ".reset", function(){
     submissions = 0;
     $('.add-number').val("");
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
